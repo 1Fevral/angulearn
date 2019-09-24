143a19bfc4f4f141cf70039c498fe627804b900e
1Fevral <olehgame@ukr.net>
2019-09-25 01:24:14 +0300

    adding items list realization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component} from '@angular/core';
-      
+ 
+class Item {
+     name: string;
+     price: number;
+
+     constructor(name: string, price: number) {
+          this.name = name;
+          this.price = price;
+     }
+}
+
 @Component({
     selector: 'my-app',
     template: `<child-comp [name]="name"></child-comp>
                 <input type="text" [(ngModel)]="name" />`
 })
 export class AppComponent{ 
-     name:string="Tom";
+     items: Item[] = 
+     [
+          {name: "Bread", price: 12}
+     ];
+     
+     name: string= this.items[0].name;
 }
